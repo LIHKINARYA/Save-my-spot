cd4e9c4736fade4612d2f9b399e9f2448edf3270
Nikhil Arya <48863874+LIHKINARYA@users.noreply.github.com>
2026-02-05 22:00:35 +0000

    Add Firebase configuration files and update pubspec.yaml

---
+++ save_my_spot_full_final/lib/firebase_options.dart
@@ -0,0 +1,14 @@
+// AUTO-GENERATED BASED ON YOUR FIREBASE CONFIG
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    return const FirebaseOptions(
+      apiKey: "AIzaSyBIhp5Vk_sOoO2ViA7gOI_cozLAnWFEqJA",
+      appId: "1:495169280763:android:54bdea984b1add1b726e5e",
+      messagingSenderId: "495169280763",
+      projectId: "save-my-spot-87b77",
+      storageBucket: "save-my-spot-87b77.firebasestorage.app",
+    );
+  }
+}
